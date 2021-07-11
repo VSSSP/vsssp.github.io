342ad22f5ba9ed75c4ed032971467fc58704b1ca
Victor de Paula <victorssspaula@live.com>
2021-07-10 23:39:58 -0300

    Botoes Projetos e Script.js adicionados

---
+++ script.js
@@ -0,0 +1,10 @@
+const projeto1 = document.querySelector('#lessons-learned');
+const projeto2 = document.querySelector('#playground-functions')
+const projeto3 = document.querySelector('#pixel-art')
+const projeto4 = document.querySelector('#todo-list')
+
+function primeiroProjeto() {
+  projeto1.addEventListener('click' () => {
+    
+  })
+}
