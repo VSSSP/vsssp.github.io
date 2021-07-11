07b285b9ce72a8eff015f2d752c176f2c658b322
Victor de Paula <victorssspaula@live.com>
2021-07-11 15:00:37 -0300

    alteracoes js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const projeto3 = document.querySelector('#pixel-art')
 const projeto4 = document.querySelector('#todo-list')
 
 function primeiroProjeto() {
-  projeto1.addEventListener('click' () => {
+  projeto1.addEventListener('click' (a) => {
     
   })
 }
